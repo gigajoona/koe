dea65266479ab84af58c034662778665b0300ae2
gigajoona <joona@dublao.com>
2012-03-21 13:00:22 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Koe::Application.config.secret_token = '48b20cf76e6638e8b3450897850019f0888935dec61d86817d53bb93fd8bb3cd7e34be421cd90eef80535ef2966e266b2b66dc184d99fb5ad63fdb8dae17bdc8'
